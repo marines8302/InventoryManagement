10740eb5e6c527e23738f5a83f95056b50aeb751
marines8302 <marines8302@gmail.com>
2020-12-10 14:24:12 +0100

    added inventory class and product class

---
+++ src/Model/Product.java
@@ -0,0 +1,96 @@
+package Model;
+
+import java.util.ArrayList;
+/**
+ * Class Product.java
+ */
+public class Product {
+    private ArrayList<Part> associatedParts = new ArrayList<>();
+    private int id;
+    private String name;
+    private double price;
+    private int stock;
+    private int min;
+    private int max;
+
+    public Product(int id, String name, double price, int stock, int min, int max) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.min = min;
+        this.max = max;
+    }
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+    /**
+     * @param name the id to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    /**
+     * @return the price
+     */
+    public double getPrice() {
+        return price;
+    }
+    /**
+     * @param price the id to set
+     */
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    /**
+     * @return the stock
+     */
+    public int getStock() {
+        return stock;
+    }
+    /**
+     * @param stock the id to set
+     */
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    /**
+     * @return the min
+     */
+    public int getMin() {
+        return min;
+    }
+    /**
+     * @param min the id to set
+     */
+    public void setMin(int min) {
+        this.min = min;
+    }
+    /**
+     * @return the max
+     */
+    public int getMax() {
+        return max;
+    }
+    /**
+     * @param max the id to set
+     */
+    public void setMax(int max) {
+        this.max = max;
+    }
+}

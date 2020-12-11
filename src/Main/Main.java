c95b506279639dd8c4c0b4f6e38fbf399160f85a
marines8302 <marines8302@gmail.com>
2020-12-11 11:13:51 +0100

    changed window size

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/View_Controller/Main.fxml"));
         primaryStage.setTitle("Inventory System Manager");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1050, 750));
         primaryStage.show();
     }
 
